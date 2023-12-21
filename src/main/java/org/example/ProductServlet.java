d377fbbedcd23cf8e69ba7734280c748d12ccd1d
Grewu <pavelgurevich97@gmail.com>
2023-12-21 10:35:11 +0300

    fix post methode

--- src/main/java/org/example/ProductServlet.java
+++ src/main/java/org/example/ProductServlet.java
@@ -1,7 +1,9 @@
 package org.example;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.example.dao.ProductDao;
 import org.example.dao.ProductDaoImpl;
 import org.example.dto.ProductDto;
@@ -9,14 +11,17 @@ import org.example.mapper.ProductMapper;
 import org.example.mapper.ProductMapperImpl;
 import org.example.service.ProductService;
 import org.example.service.ProductServiceImpl;
+import org.example.util.LocalDateTimeDeserializer;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @WebServlet(value = "/controller")
@@ -24,6 +29,7 @@ public class ProductServlet extends HttpServlet {
     private final ProductMapper productMapper = new ProductMapperImpl();
     private final ProductDao productDao = new ProductDaoImpl(productMapper);
     private final ProductService productService = new ProductServiceImpl(productMapper, productDao);
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -42,8 +48,28 @@ public class ProductServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ProductDto productDto = new Gson().fromJson(req.getReader().toString(), ProductDto.class);
-        String json = new Gson().toJson(productDto);
+        BufferedReader reader = req.getReader();
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append('\n');
+            }
+        } finally {
+            reader.close();
+        }
+
+        String json = stringBuilder.toString();
+
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeDeserializer())
+                .create();
+
+        ProductDto productDto = gson.fromJson(json, ProductDto.class);
+        productService.create(productDto);
+
         try (PrintWriter out = resp.getWriter()) {
             out.write(json);
             resp.setStatus(200);
